9f5834b41949ed94d123fefa04207ca0870bef1c
lorena morais <lorenamorais@alunos.utfpr.edu.br>
2022-05-20 17:54:15 -0300

    feat: Validations choice

--- src/controllers/choiceController.js
+++ src/controllers/choiceController.js
@@ -1,15 +1,43 @@
 import db from "../db.js";
+import Joi from "joi";
+import { ObjectId } from "mongodb";
+
+const choiceSchema = Joi.object({
+    title: Joi.string().required().trim(),
+    poolId: Joi.required()
+});
 
 export async function setChoice(req, res){
+    const validation = choiceSchema.validate(req.body);
+
+    if(validation.error){
+        res.sendStatus(422);
+        return
+    }
+    
     const choice = {
         title: req.body.title,
         poolId: req.body.poolId
     }
+    console.log(choice.poolId);
+    console.log(new ObjectId(choice.poolId));
 
     try{
+        const searchPoll = await db.collection('poll').findOne({ _id: new ObjectId(choice.poolId)});
+
+        if(!searchPoll){
+            return res.status(404).send('Enquete não encontrada!');
+        }
+
+        const searchChoice = await db.collection('choice').findOne({title: choice.title});
+
+        if(searchChoice){
+            return res.status(409).send('Voto já computado!');
+        }
+
         await db.collection('choice').insertOne(choice);
-        res.sendStatus(201);
+        res.status(201).send(choice);
     }catch(error){
-        res.sendStatus(500);
+        res.sendStatus(500).send(error.message);
     }
 }
