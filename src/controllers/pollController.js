027335b4f9c3cd2451a87d94b73d7b581c5213da
lorena morais <lorenamorais@alunos.utfpr.edu.br>
2022-05-23 01:47:58 -0300

    feat: results

--- src/controllers/pollController.js
+++ src/controllers/pollController.js
@@ -55,3 +55,40 @@ export async function getChoiceOptions(req, res){
         res.sendStatus(500);
     }
 }
+
+export async function countVotes(req, res){
+    const id = req.params.id;
+
+    try{
+        const choice = await db.collection('choice').find({ poolId: id}).toArray();
+        const vote = await db.collection('vote').find({ }).toArray();
+        const counter = [];
+        let p = 0;
+
+        for(let i = 0; i < choice.length; i++){
+            counter.push(0);
+        }
+
+        for(let i = 0; i < choice.length; i++){
+            for( let j = 0; j < vote.length; j++){
+                if(choice[i]._id == (new ObjectId(vote[j].choiceId).toString())){
+                    counter[i]++;
+                    p = i;
+                }
+            }
+        }
+
+        const poll = await db.collection('poll').find({ _id: new ObjectId(id) }).toArray();
+
+        res.send({
+            ...poll,
+            result: {
+                title: choice[p].title,
+                votes: Math.max(...counter)
+            }
+        });
+    }catch(error){
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
--- src/routes/pollRouter.js
+++ src/routes/pollRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import {setPoll, getPoll, getChoiceOptions} from '../controllers/pollController.js';
+import {setPoll, getPoll, getChoiceOptions, countVotes} from '../controllers/pollController.js';
 
 const pollRouter = express.Router();
 
 pollRouter.post('/poll', setPoll);
 pollRouter.get('poll', getPoll);
-pollRouter.get('/poll/:id/choice', getChoiceOptions)
+pollRouter.get('/poll/:id/choice', getChoiceOptions);
+pollRouter.get('/poll/:id/result', countVotes);
 
 export default pollRouter;
